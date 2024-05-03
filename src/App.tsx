1bb09f80ac096cffbf2ef18f3206e44bec2794f6
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-05-03 14:09:44 +0200

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,51 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Navbar from './Header/Navbar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Planing from './Components/Planing';
+import Statistics from './Components/Statistics';
+import Login from './Components/Login';
+import Admin from './Components/Admin';
 
-import './App.css'
+
 
 function App() {
+  const [page, setPage] = useState<string>('');
+
+  useEffect(() => {
+    let pageUrl = page;
+
+    if (!pageUrl) {
+      const queryparam = new URLSearchParams(window.location.search);
+      const getUrl = queryparam.get('page');
+
+      if (getUrl) {
+        pageUrl = getUrl;
+        setPage(getUrl);
+      } else {
+        pageUrl = 'home';
+      }
+    }
+
+    window.history.pushState(null, '', `?page=${pageUrl}`);
+  }, [page]);
 
   return (
     <>
-
+    <div>
+      <Navbar setPage={setPage} />
+      {{
+        "homePage": <Home/>,
+        "about": <About/>,
+        "planing": <Planing/>,
+        "statistics": <Statistics/>,
+        "admin": <Admin/>,
+        "login": <Login/>
+      }[page]}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Admin.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Admin() {
+  return (
+    <div>
+      <h1>Förbjuden mark</h1>
+    </div>
+  )
+}
+
+export default Admin
