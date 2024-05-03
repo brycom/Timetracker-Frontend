4021fd1eb849a5aece2f3981b92bd90be0d57f99
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-05-03 12:26:34 +0200

    start

---
+++ src/navbar/navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function navbar() {
+  return (
+    <div>
+        <button>Hem</button>
+        <button>Om oss</button>
+        <button>Planering</button>
+        <button>Statestik</button>
+        <button>Admin</button>
+        <button>Logga in</button>
+      
+    </div>
+  )
+}
+
+export default navbar
